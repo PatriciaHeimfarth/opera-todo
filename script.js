12053eaa0a2f51ede8545d7f3687d97c434ec7a7
Patti85 <pbeier24@googlemail.com>
2021-02-14 17:33:07 +0100

    append rows

--- script.js
+++ script.js
@@ -5,22 +5,14 @@ function addRow() {
     var rowCount = table.rows.length;
     var row = table.insertRow(rowCount);
 
-    var cell1 = row.insertCell(0);
+    var cell = row.insertCell(0);
+    cell.innerHTML = 'ola';
+
+    var cell1 = row.insertCell(1);
     var element1 = document.createElement("input");
     element1.type = "checkbox";
-    element1.name = "chkbox[]";
-    cell1.appendChild(element1);
-
-    var cell2 = row.insertCell(1);
-    cell2.innerHTML = rowCount + 1;
-
-    var cell3 = row.insertCell(2);
-    var element2 = document.createElement("input");
-    element2.type = "text";
-    element2.name = "txtbox[]";
-    cell3.appendChild(element2);
-
-
+    cell1.append(element1)
+ 
 }
 
 window.onload = function () {
