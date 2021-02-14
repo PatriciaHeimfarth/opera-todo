0d4210aa80205270859053a4d0678831a336ebf7
Patti85 <pbeier24@googlemail.com>
2021-02-14 20:07:39 +0100

    load from inner storage works

--- script.js
+++ script.js
@@ -6,16 +6,29 @@ function addRow() {
     var row = table.insertRow(rowCount);
 
     var cell = row.insertCell(0);
+    
     cell.innerHTML = document.getElementById("textfield").value;
 
     var cell1 = row.insertCell(1);
     var element1 = document.createElement("input");
     element1.type = "checkbox";
     cell1.append(element1)
- 
+
 }
 
 window.onload = function () {
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+
+    var cell3 = row.insertCell(0);
+     
+    cell3.innerHTML = localStorage.getItem("lastname");
+   
+
+    var cell4 = row.insertCell(1);
+    var element4 = document.createElement("input");
+    element4.type = "checkbox";
+    cell4.append(element4)
     document.getElementById("button").onclick = addRow;
 }
 
